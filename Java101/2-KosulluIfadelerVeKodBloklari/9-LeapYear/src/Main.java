ec3a50cbda2772393fa66a24854306d025802ed6
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-11 10:57:32 +0300

    Leap Year code added

--- Java101/2-KosulluIfadelerVeKodBloklari/9-LeapYear/src/Main.java
+++ Java101/2-KosulluIfadelerVeKodBloklari/9-LeapYear/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        int year;
+
+        System.out.print("Yıl giriniz : ");
+        year = input.nextInt();
+
+        if (year % 400 == 0 || year % 4 == 0) {
+            System.out.println(year + " Bir Artık Yıldır.");
+        } else {
+            System.out.println(year + " Bir artık yıl değildir");
+        }
+
     }
 }
