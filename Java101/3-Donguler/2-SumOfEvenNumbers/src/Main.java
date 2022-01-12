ecf943d1a228b8646f30c9fcda6f1a58bef2ff2e
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-12 16:46:04 +0300

    sum of even number example code added

--- Java101/3-Donguler/2-SumOfEvenNumbers/src/Main.java
+++ Java101/3-Donguler/2-SumOfEvenNumbers/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int number, sum = 0;
+
+        System.out.print("Pozitif Tam Sayı Giriniz : ");
+        number = input.nextInt();
+
+        if (number > 0) {
+
+            for (int i = 0; i <= number; i += 2) {
+                sum += i;
+            }
+            System.out.println("Girilen Değerin 2 ve 4 Katlarının Toplamı : " + sum);
+
+
+        } else {
+            System.out.println("Pozitif Tam Sayı Girilmedi!");
+        }
 
     }
 }
