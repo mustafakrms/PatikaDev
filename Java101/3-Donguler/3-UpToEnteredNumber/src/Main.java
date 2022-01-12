dae06a1105e8e0e29c0594e6b6178bd6f1e2616f
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-12 19:14:33 +0300

    code added.

--- Java101/3-Donguler/3-UpToEnteredNumber/src/Main.java
+++ Java101/3-Donguler/3-UpToEnteredNumber/src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Sınır Sayısını Giriniz : ");
+        number = input.nextInt();
+
+        System.out.println("4'ün katı Olanlar");
+        System.out.println("-----------------");
+
+        for (int i = 1; i <= number; i *= 4) {
+
+            System.out.print(i + " - ");
+        }
+        System.out.println("\n-----------------");
+        System.out.println("5'ün katı Olanlar");
+        System.out.println("-----------------");
+
+        for (int i = 1; i <= number; i *= 5) {
+            System.out.print(i + " - ");
+        }
+    }
 }
