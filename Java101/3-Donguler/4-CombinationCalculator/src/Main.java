61b28bb9ea100c2bd6cb45f2d9fcaa76997e5c93
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 00:04:54 +0300

    combination code added

--- Java101/3-Donguler/4-CombinationCalculator/src/Main.java
+++ Java101/3-Donguler/4-CombinationCalculator/src/Main.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int numberOfElement, numberOfChoises, elementFactorial = 1, count = 1, choicesFactorial = 1, subtraction = 1, combination;
+
+        System.out.print("Eleman Sayısını Girin (n) : ");
+        numberOfElement = input.nextInt();
+
+        System.out.print("Seçim Sayısını Girin (r) : ");
+        numberOfChoises = input.nextInt();
+
+
+        if (numberOfElement >= numberOfChoises) {
+
+            for (int i = 1; i <= numberOfElement; i++) {
+                elementFactorial *= i;
+                if (i <= numberOfChoises) {
+                    choicesFactorial *= i;
+                } else {
+                    subtraction *= count;
+                    count++;
+                }
+            }
+            combination = elementFactorial / (choicesFactorial * subtraction);
+            System.out.printf("C( %d , %d ) = %d ", numberOfElement, numberOfChoises, combination);
+
+        } else {
+            System.out.println("Seçim Sayısı Eleman Sayısından Küçük Olamaz!");
+        }
+
+
+    }
 }
