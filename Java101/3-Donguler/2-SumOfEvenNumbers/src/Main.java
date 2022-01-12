12148d509b9cff718bf5b2650633ff1b3dfcca2c
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-12 18:46:19 +0300

    code fixed

--- Java101/3-Donguler/2-SumOfEvenNumbers/src/Main.java
+++ Java101/3-Donguler/2-SumOfEvenNumbers/src/Main.java
@@ -6,20 +6,16 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int number, sum = 0;
 
-        System.out.print("Pozitif Tam Sayı Giriniz : ");
-        number = input.nextInt();
 
-        if (number > 0) {
-
-            for (int i = 0; i <= number; i += 2) {
-                sum += i;
+        do {
+            System.out.print("Pozitif Sayı Giriniz : ");
+            number = input.nextInt();
+            if (number % 4 == 0 && number > 0) {
+                sum += number;
             }
-            System.out.println("Girilen Değerin 2 ve 4 Katlarının Toplamı : " + sum);
 
-
-        } else {
-            System.out.println("Pozitif Tam Sayı Girilmedi!");
-        }
+        } while (number % 2 == 0);
+        System.out.println("Girilen sayılardan 2 ve 4 katı olanların toplamı = " + sum);
 
     }
 }
