44d3cd803f7decb72e120d017ea1cf0a2e6b1401
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 22:13:32 +0300

    Perfect Number code added

--- Java101/3-Donguler/12-PerfectNumber/src/Main.java
+++ Java101/3-Donguler/12-PerfectNumber/src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Pozitif Tam Sayı Giriniz : ");
+        int number = input.nextInt();
+        int sum = 0;
+
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    sum += i;
+                }
+            }
+            if (sum == (number * 2)) {
+                System.out.printf("%d Sayısı Mükemmel Sayıdır.\n", number);
+            } else {
+                System.out.printf("%d Sayısı Mükemmel Sayı Değildir.\n", number);
+            }
+
+        } else {
+            System.out.println("Hatalı Değer Girdiniz!");
+        }
+    }
 }
