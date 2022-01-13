93c66cc860316d31b97bfa6eb6e2c1e16c2dabf6
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 05:00:44 +0300

    exponents calculator added

--- Java101/3-Donguler/5-ExponentsCalculator/src/Main.java
+++ Java101/3-Donguler/5-ExponentsCalculator/src/Main.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number, exponent, total = 1;
+
+        System.out.print("Üssü Alınacak Sayıyı Giriniz : ");
+        number = input.nextInt();
+
+        System.out.print("Üs Olacak Sayıyı Giriniz : ");
+        exponent = input.nextInt();
+
+        for (int i = 1; i <= exponent; i++) {
+
+            total *= number;
+
+        }
+        System.out.printf("%d^%d = %d", number, exponent, total);
+
+    }
 }
