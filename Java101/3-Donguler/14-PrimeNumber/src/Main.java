8dd69ea9de8987ea87e92789d5214c58c492e254
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 23:02:57 +0300

    fibanocci series code added

--- Java101/3-Donguler/14-PrimeNumber/src/Main.java
+++ Java101/3-Donguler/14-PrimeNumber/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         for (int i = 2; i <= 100; i++) {
             int count = -1;
             for (int j = 2; j <= 100; j++) {
--- Java101/3-Donguler/15-FibanocciSeries/src/Main.java
+++ Java101/3-Donguler/15-FibanocciSeries/src/Main.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Basamak Sayısını Giriniz : ");
+        int digit = input.nextInt();
+        int numberOne = 0, numberTwo = 1, numberThree;
+
+        System.out.printf("%d Basamaklı Fibanocci Serisi : ", digit);
+        for (int i = 0; i < digit; i++) {
+
+            numberThree = numberOne + numberTwo;
+            System.out.printf("%d ", numberOne);
+            numberOne = numberTwo;
+            numberTwo = numberThree;
+
+        }
+
+    }
 }
