36ecd17c2f094bf2aed56fad0bf8ddc79d403e55
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 22:24:07 +0300

    reverse triaangle code added

--- Java101/3-Donguler/13-ReverseTriangle/src/Main.java
+++ Java101/3-Donguler/13-ReverseTriangle/src/Main.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Pozitif Tam Sayı Giriniz : ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            //Satır Sayısı
+            for (int i = 1; i <= number; i++) {
+
+                //Boşluk Bırakan Döngü
+                for (int j = 1; j < i; j++) {
+                    System.out.print(" ");
+                }
+                //Yıldız Bırakan Döngü
+                for (int k = 0; k < (2 * (number - i)) + 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        } else {
+            System.out.println("Hatalı Değer Girdiniz!");
+        }
+    }
 }
