23e36cde67cd8bf94dfdce62c76a31972bebad5e
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 05:53:20 +0300

    sum of digit number code added

--- Java101/3-Donguler/6-SumOfDigits/src/Main.java
+++ Java101/3-Donguler/6-SumOfDigits/src/Main.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        //   # Bir sayının basamak sayılarının toplamını hesaplayan program
+
+
+        Scanner input = new Scanner(System.in);
+        int number, tempNumber, sum = 0;
+
+        System.out.print("Sayı Giriniz : ");
+        number = input.nextInt();
+
+        tempNumber = number;
+        while (tempNumber != 0) {
+            sum += (tempNumber % 10);
+            tempNumber /= 10;
+        }
+        System.out.printf("%d Sayısının Basamak Sayılarının Toplamı : %d", number, sum);
+
+    }
 }
