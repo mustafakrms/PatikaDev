ddc0be42dfebf9ad212f6b13c194668934974797
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 07:28:07 +0300

    harmonic series code added

--- Java101/3-Donguler/7-FindTheHarmonicSeries/src/Main.java
+++ Java101/3-Donguler/7-FindTheHarmonicSeries/src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("N Sayısını Giriniz : ");
+        int number = input.nextInt();
+        double result = 0.0;
+
+        for (double i = 1; i <= number; i++) {
+            result += (1 / i);
+        }
+
+        System.out.printf("%d Sayısının Harmonik Serisi : %f", number, result);
+    }
 }
