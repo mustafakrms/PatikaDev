cef95041cc2bf63b4172caf4e00a827157cc8c01
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-13 22:41:53 +0300

    Prime number code added

--- Java101/3-Donguler/14-PrimeNumber/src/Main.java
+++ Java101/3-Donguler/14-PrimeNumber/src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        
+        for (int i = 2; i <= 100; i++) {
+            int count = -1;
+            for (int j = 2; j <= 100; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+    }
 }
