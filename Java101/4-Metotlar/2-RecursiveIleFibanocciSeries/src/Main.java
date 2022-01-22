bd530835f4cbeede195eea3da9bbc4426c7f3ed8
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-22 04:25:52 +0300

    fibanocci serisi kodu eklendi

--- Java101/4-Metotlar/2-RecursiveIleFibanocciSeries/src/Main.java
+++ Java101/4-Metotlar/2-RecursiveIleFibanocciSeries/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int fib(int n) {
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Fibanocci Serisinde Bumak İstediğiniz Basamağı Giriniz : ");
+        int number = input.nextInt();
+
+        System.out.printf("%d. Basamakta %d Sayısı Bulunuyor.", number, fib(number));
 
     }
 }
