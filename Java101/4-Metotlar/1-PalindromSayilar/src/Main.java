969d5de13cc81bfc897a789d4aa9079c4ec5534c
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-22 04:12:14 +0300

    polindrom sayılar kodu eklendi

--- Java101/4-Metotlar/1-PalindromSayilar/src/Main.java
+++ Java101/4-Metotlar/1-PalindromSayilar/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static boolean isPolindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber)
+            return true;
+        else
+            return false;
+
+
+    }
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz : ");
+        int number = input.nextInt();
+
+        if (isPolindrom(number))
+            System.out.println("Girdiğiniz Sayı Polindrom Sayıdır.");
+        else
+            System.out.println("Gidiğiniz Sayı Polindrom Sayı Değildir!");
+
 
     }
 }
