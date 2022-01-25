4aad36bf5e0fe2a7a594ee2e638edc22058eda99
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-25 18:32:57 +0300

    desene göre metot eklendi

--- Java101/4-Metotlar/6-DeseneGoreMetot/src/Main.java
+++ Java101/4-Metotlar/6-DeseneGoreMetot/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı Giriniz : ");
+        int number = input.nextInt();
+
+        patternMethod(number, number, false);
+    }
+
+    static int patternMethod(int number, int temp, boolean isState) {
+
+        if (number > 0 && !(isState)) {
+            System.out.print(number + " ");
+            number -= 5;
+            patternMethod(number, temp, false);
+
+        } else if (temp >= number) {
+            System.out.print(number + " ");
+            number += 5;
+            patternMethod(number, temp, true);
+        }
+        return 1;
 
     }
 }
