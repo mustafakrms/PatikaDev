46ae1e390146c49d2fb117f40a199fecb64e67b0
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-25 17:10:09 +0300

    üs hesabı kodu eklendi.

--- Java101/4-Metotlar/4-UsHesabi/src/Main.java
+++ Java101/4-Metotlar/4-UsHesabi/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Taban Değer Giriniz : ");
+        int number = input.nextInt();
+        System.out.print("Üs Değerini Giriniz : ");
+        int expo = input.nextInt();
+
+        int total = 1;
+        int result = exponentialNumber(number, expo, total);
+        System.out.println(number + "^" + expo + " = " + result);
+
+    }
+
+    static int exponentialNumber(int number, int expo, int total) {
+        expo--;
+        if (expo >= 0) {
+            total *= number;
+            return exponentialNumber(number, expo, total);
+        }
+        return total;
     }
 }
