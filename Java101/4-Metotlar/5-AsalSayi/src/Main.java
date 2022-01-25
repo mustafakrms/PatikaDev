ef3db85af454e63102753f31bf84b788604ac60a
Mustafa Karamese <karamesemustafa24@gmail.com>
2022-01-25 18:31:56 +0300

    asal sayı programı eklendi

--- Java101/4-Metotlar/5-AsalSayi/src/Main.java
+++ Java101/4-Metotlar/5-AsalSayi/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Giriniz : ");
+        int number = input.nextInt();
+
+        if (isPrimeNumber(number, number)) {
+            System.out.println(number + " Sayısı ASALDIR.");
+        } else {
+            System.out.println(number + " Sayısı ASAL DEĞİLDİR.");
+        }
+
+    }
+
+    static boolean isPrimeNumber(int number, int counter) {
+        counter--;
+        if (counter > 1) {
+            if (number % counter == 0) {
+                return false;
+            } else {
+                return isPrimeNumber(number, counter);
+            }
+        } else {
+            return true;
+        }
+
 
     }
 }
